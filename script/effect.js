aa4cbc7d26008e2aba0498cd4ab0d2b27b5ba776
min0622 <151921634+min0622@users.noreply.github.com>
2024-03-19 15:51:40 +0900

    Add files via upload

--- script/effect.js
+++ script/effect.js
@@ -30,6 +30,7 @@ $(function(){
 });
 
 
+
 $('#wrap .section_3').slick({
     dots: true,
     infinite: false,
@@ -49,27 +50,55 @@ $('#wrap .section_3').slick({
   });
 
 
-// $('#wrap .section_3').slick({
-//     dots: true,
-//     infinite: true,
-//     speed: 300,
-//     slidesToShow: 1,
-//     adaptiveHeight: true
 
-//     responsive: [
-       
-//         {
-//           breakpoint: 576,
-//           settings:"unslick"
-//         }
-//         // You can unslick at a given breakpoint now by adding:
-//         // settings: "unslick"
-//         // instead of a settings object
-//       ]
+  $('#wrap .section_6 .review').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+      }
+      },
+      {
+        breakpoint: 969,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+      }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+      }
+      },
 
 
-//   });
 
+
+
+
+
+
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 
 
 
