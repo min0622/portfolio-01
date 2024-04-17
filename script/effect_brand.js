94c1c15999f28b13f0910a425fac7d194f39201f
min0622 <151921634+min0622@users.noreply.github.com>
2024-04-17 13:03:02 +0900

    Add files via upload

--- script/effect_brand.js
+++ script/effect_brand.js
@@ -11,22 +11,54 @@ $(function(){
   });
 
 
+//section5
 
-    $("#sub1_wrap .s1_section5 .s_btn .s_btnBox").on({
+    $("#sub1_wrap .s1_section5 .s_btn .b1").on({
 
         mouseover:function(){
           $(this).stop(true).animate({backgroundColor:'#000'}),
-          $('.c').stop(true).animate({color:'#fff'}),
-          $('.a').css({opacitiy:'0'}).stop(true).attr('src','./img/web/sub1/right_w.png').animate({opacity:'1'},300,'easeInOutQuad')
+          $('.c1').stop(true).animate({color:'#fff'}),
+          $('.a1').css({opacitiy:'0'}).stop(true).attr('src','./img/web/sub1/right_w.png').animate({opacity:'1'},300,'easeInOutQuad')
 
         },
         mouseout:function(){
             $(this).stop(true).animate({backgroundColor:'#fff'}),
-          $('.c').stop(true).animate({color:'#000'}),
-          $('.a').css({opacity:'0'}).stop(true).attr('src','./img/web/sub1/right.png').animate({opacity:'1'},300,'easeInOutQuad')
+          $('.c1').stop(true).animate({color:'#000'}),
+          $('.a1').css({opacity:'0'}).stop(true).attr('src','./img/web/sub1/right.png').animate({opacity:'1'},300,'easeInOutQuad')
 
         }
+      })
 
+    $("#sub1_wrap .s1_section5 .s_btn .b2").on({
+
+        mouseover:function(){
+          $(this).stop(true).animate({backgroundColor:'#000'}),
+          $('.c2').stop(true).animate({color:'#fff'}),
+          $('.a2').css({opacitiy:'0'}).stop(true).attr('src','./img/web/sub1/right_w.png').animate({opacity:'1'},300,'easeInOutQuad')
+
+        },
+        mouseout:function(){
+            $(this).stop(true).animate({backgroundColor:'#fff'}),
+          $('.c2').stop(true).animate({color:'#000'}),
+          $('.a2').css({opacity:'0'}).stop(true).attr('src','./img/web/sub1/right.png').animate({opacity:'1'},300,'easeInOutQuad')
+
+        }
+      })
+   
+    $("#sub1_wrap .s1_section5 .s_btn .b3").on({
+
+        mouseover:function(){
+          $(this).stop(true).animate({backgroundColor:'#000'}),
+          $('.c3').stop(true).animate({color:'#fff'}),
+          $('.a3').css({opacitiy:'0'}).stop(true).attr('src','./img/web/sub1/right_w.png').animate({opacity:'1'},300,'easeInOutQuad')
+
+        },
+        mouseout:function(){
+            $(this).stop(true).animate({backgroundColor:'#fff'}),
+          $('.c3').stop(true).animate({color:'#000'}),
+          $('.a3').css({opacity:'0'}).stop(true).attr('src','./img/web/sub1/right.png').animate({opacity:'1'},300,'easeInOutQuad')
+
+        }
       })
    
       
@@ -37,3 +69,4 @@ $(function(){
 
 
 });
+
