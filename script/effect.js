fd57a9d723b9d12df5d22da99b2aecdfc75b4acd
min0622 <151921634+min0622@users.noreply.github.com>
2024-05-07 15:13:21 +0900

    Add files via upload

--- script/effect.js
+++ script/effect.js
@@ -20,10 +20,10 @@ $('.x').click(function(){
   $('#wrap .section_2 .itemBox .i1 .imgBox').on({
 
     mouseover: function () {
-      $('.m2-1').css({ opacity: '0' }).stop(true).attr('src', 'img/web/main/hmain2-1.png').animate({ opacity: '1' }, 1600, 'easeOutQuint')
+      $('.m2-1').css({ opacity: '0' }).stop(true).attr('src', 'img/web/main/hmain2-1.png').animate({ opacity: '1' }, 900, 'easeOutSine')
     },
     mouseout: function () {
-      $('.m2-1').css({ opacity: '0' }).stop(true).attr('src', 'img/web/main/main2-1.png').animate({ opacity: '1'}, 1600, 'easeOutQuint')
+      $('.m2-1').css({ opacity: '0' }).stop(true).attr('src', 'img/web/main/main2-1.png').animate({ opacity: '1'}, 900, 'easeOutSine')
 
     }
   });
@@ -31,47 +31,47 @@ $('.x').click(function(){
   $('#wrap .section_2 .itemBox .i2 .imgBox').on({
 
     mouseover: function () {
-      $('.m2-2').css({ opacity: '0' }).stop(true).attr('src', 'img/web/main/hmain2-2.png').animate({ opacity: '1' }, 1600, 'easeOutQuint')
+      $('.m2-2').css({ opacity: '0' }).stop(true).attr('src', 'img/web/main/hmain2-2.png').animate({ opacity: '1' }, 900, 'easeOutSine')
     },
 
     mouseout: function () {
-      $('.m2-2').css({ opacity: '0' }).stop(true).attr('src', 'img/web/main/main2-2.png').animate({ opacity: '1' }, 1600, 'easeOutQuint')
+      $('.m2-2').css({ opacity: '0' }).stop(true).attr('src', 'img/web/main/main2-2.png').animate({ opacity: '1' }, 900, 'easeOutSine')
     }
   });
 
   $('#wrap .section_2 .itemBox .i3 .imgBox').on({
     mouseover:function(){
-      $('.m2-3').css({opacity:'0'}).stop(true).attr('src','img/web/main/hmain2-3.png').animate({opacity:'1'},1600, 'easeOutQuint')
+      $('.m2-3').css({opacity:'0'}).stop(true).attr('src','img/web/main/hmain2-3.png').animate({opacity:'1'},900, 'easeOutSine')
     },
     mouseout:function(){
-      $('.m2-3').css({opacity:'0'}).stop(true).attr('src','img/web/main/main2-3.png').animate({opacity:'1'},1600, 'easeOutQuint')
+      $('.m2-3').css({opacity:'0'}).stop(true).attr('src','img/web/main/main2-3.png').animate({opacity:'1'},900, 'easeOutSine')
     }
   });
 
   $('#wrap .section_2 .itemBox .i4 .imgBox').on({
     mouseover:function(){
-      $('.m2-4').css({opacity:'0'}).stop(true).attr('src','img/web/main/hmain2-4.png').animate({opacity:'1'},1600, 'easeOutQuint')
+      $('.m2-4').css({opacity:'0'}).stop(true).attr('src','img/web/main/hmain2-4.png').animate({opacity:'1'},900, 'easeOutSine')
     },
     mouseout:function(){
-      $('.m2-4').css({opacity:'0'}).stop(true).attr('src','img/web/main/main2-4.png').animate({opacity:'1'},1600, 'easeOutQuint')
+      $('.m2-4').css({opacity:'0'}).stop(true).attr('src','img/web/main/main2-4.png').animate({opacity:'1'},900, 'easeOutSine')
     }
   });
 
   $('#wrap .section_2 .itemBox .i5 .imgBox').on({
     mouseover:function(){
-      $('.m2-5').css({opacity:'0'}).stop(true).attr('src','img/web/main/hmain2-5.png').animate({opacity:'1'},1600, 'easeOutQuint')
+      $('.m2-5').css({opacity:'0'}).stop(true).attr('src','img/web/main/hmain2-5.png').animate({opacity:'1'},900, 'easeOutSine')
     },
     mouseout:function(){
-      $('.m2-5').css({opacity:'0'}).stop(true).attr('src','img/web/main/main2-5.png').animate({opacity:'1'},1600, 'easeOutQuint')
+      $('.m2-5').css({opacity:'0'}).stop(true).attr('src','img/web/main/main2-5.png').animate({opacity:'1'},900, 'easeOutSine')
     }
   });
 
   $('#wrap .section_2 .itemBox .i6 .imgBox').on({
     mouseover:function(){
-      $('.m2-6').css({opacity:'0'}).stop(true).attr('src','img/web/main/hmain2-6.png').animate({opacity:'1'},1600, 'easeOutQuint')
+      $('.m2-6').css({opacity:'0'}).stop(true).attr('src','img/web/main/hmain2-6.png').animate({opacity:'1'},900, 'easeOutSine')
     },
     mouseout:function(){
-      $('.m2-6').css({opacity:'0'}).stop(true).attr('src','img/web/main/main2-6.png').animate({opacity:'1'},1600, 'easeOutQuint')
+      $('.m2-6').css({opacity:'0'}).stop(true).attr('src','img/web/main/main2-6.png').animate({opacity:'1'},900, 'easeOutSine')
     }
   });
 
@@ -91,7 +91,7 @@ $('.section_3>.slide>a>img').on({
 })
 
 
-
+// section2  슬라이드
   $('#wrap .section_2 .itemBox').slick({
     centerMode: false,
     centerPadding: '10px',
